55f9f740b5be0335c73dc3bf42bad76aeea97824
Nicolas Aoki <nick_aoki@hotmail.com>
2021-03-17 03:58:40 -0300

    WIP

---
+++ leonids/src/components/footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer className="my-12 text-center">
+      © 2021, poemas e tech... é isso
+    </footer>
+  )
+}
+
+export default Footer
